6d41aa18893c29d1b42a4150ca6defed789d5d62
zjoart <oluwadahunsiifeoluwa02@gmail.com>
2025-12-06 22:20:45 +0100

    chore: adds swagger documentation, loads services in main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,8 +5,12 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"github.com/zjoart/docai/internal/config"
 	"github.com/zjoart/docai/internal/database"
+	"github.com/zjoart/docai/internal/documents"
+	"github.com/zjoart/docai/internal/documents/analyzer"
 	"github.com/zjoart/docai/internal/storage"
 )
 
@@ -30,6 +34,20 @@ func main() {
 	if err := minioClient.EnsureBucket(context.Background()); err != nil {
 		log.Fatalf("Failed to ensure bucket: %v", err)
 	}
+
+	aiAnalyzer := analyzer.NewAnalyzer(cfg.OpenRouterAPIKey)
+
+	repo := documents.NewRepository(db)
+	svc := documents.NewService(repo, minioClient, aiAnalyzer)
+	handler := documents.NewHandler(svc)
+
+	r := mux.NewRouter()
+	documents.RegisterRoutes(r, handler)
+
+	r.PathPrefix("/docs/").Handler(http.StripPrefix("/docs/", http.FileServer(http.Dir("./docs"))))
+	r.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:8080/docs/swagger.yaml"),
+	))
 
 	log.Printf("Server starting on port %s", cfg.Port)
 	if err := http.ListenAndServe(":"+cfg.Port, r); err != nil {
